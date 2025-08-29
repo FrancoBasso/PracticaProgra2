e2a96b74e9497156c54ac6677f0b25add081ffbf
unknown <francobasso2001@gmail.com>
2025-08-28 21:04:38 -0300

    continuacion practicainicial

---
+++ TP2/E2/E2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void ordenadas(int *x, int *y)
+{
+    int aux;
+    if (*x > *y)
+    {
+        aux = *x;
+        *x = *y;
+        *y = aux;
+    }
+}
+int main()
+{
+    int a = 30, b = 20;
+    ordenadas(&a, &b);
+    printf(" valor de a %d\tvalor de b %d\n", a, b);
+    return 0;
+}
