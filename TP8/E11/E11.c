b3be2689a874f4cbdbe964fbb46b13ed32db7297
FrancoBasso <francobasso2001@gmail.com>
2025-11-06 10:18:50 -0300

    recorrido prof y amplitud corregido mat adj

--- TP8/E11/E11.c
+++ TP8/E11/E11.c
@@ -25,7 +25,7 @@ void RecorridoProfundidad(int mat[][MAX], int vv[], int N)
     TElementoP V;
     int cont = 0;
     IniciaP(&P);
-    int j, vertAct, i, flag;
+    int j, vertAct, i;
     for (i = 0; i < N; i++)
     {
         if (vv[i] == 0)
@@ -39,15 +39,9 @@ void RecorridoProfundidad(int mat[][MAX], int vv[], int N)
             {
                 vertAct = consultaP(P);
                 j = 0;
-                flag = 0;
+                
                 while (j < N && (mat[vertAct][j] == 0 || vv[j] == 1))
-                {
                     j++;
-                    if (mat[vertAct][j] == 1)
-                        flag = 1;
-                }
-                if (flag)
-                    cont++;
                 if (j < N)
                 {
                     if (vv[j] == 0)
@@ -60,6 +54,8 @@ void RecorridoProfundidad(int mat[][MAX], int vv[], int N)
                 else
                     sacaP(&P, &V);
             }
+            if (!todosVisitados(vv,N) && VaciaP(P))
+                cont++;
         } // preguntar por la condicion de conexa
     }
     printf("la cantidad de componentes conexas es : %d", cont);
@@ -68,26 +64,31 @@ void RecorridoAmplitud(int mat[][MAX], int vv[], int N)
 {
     TElementoC V;
     TCola C;
-    int vertAct, j;
+    int vertAct, j,cont=0,i;
     IniciaC(&C);
-    vertAct = 0;
+    for (i = 0; i < N; i++)
+    {
+    vertAct = i;
     printf(" %d", vertAct);
     vv[vertAct] = 1;
     poneC(&C, vertAct);
-    while (!Todosvisit(vv, N) && !VaciaC(C))
+    while (!VaciaC(C))
     {
         sacaC(&C, &vertAct);
-
+         printf("%d", vertAct);
         j = 0;
         while (j < N)
         {
             if (mat[vertAct][j] == 1 && vv[j] == 0)
             {
-                printf("%d", j);
-                poneC(&C, j);
-                vv[j] = 1;
+               
+                poneC(&C, j);vv[j] = 1;
             }
             j++;
         }
     }
+   if (!todoVisitados(vv,N) && VaciaC(C))
+       cont++;
+}
+     printf("la cantidad de componentes conexas es : %d", cont);
 }
